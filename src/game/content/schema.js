00d37dea7577d11b9441a366112ece2ccac2b17b
NovakB <novak_is@live.com>
2025-09-30 19:36:13 +0200

    refactor: remove hustle requirement summary renderer

--- src/game/content/schema.js
+++ src/game/content/schema.js
@@ -830,10 +830,6 @@ export function createUpgrade(config) {
   return definition;
 }
 
-export function renderHustleRequirementSummary(requirements) {
-  return summarizeAssetRequirements(requirements);
-}
-
 export function hustleRequirementsMet(requirements) {
   return meetsAssetRequirements(requirements);
 }
